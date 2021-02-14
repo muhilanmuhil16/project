0970428e71c34a5355ddfc16dcbfab3a6740f355
Muhilan Venkateswaran <75116043+muhilanmuhil16@users.noreply.github.com>
2021-02-14 05:35:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+gsap.to("#stle",{
+    scrollTrigger:{
+        scrub:true
+    },
+    scaleY:.5,
+})
+gsap.to("#titl",{
+    scrollTrigger:{
+        scrub:true
+    },
+    scaleY:0.9,
+    opacity:5,
+})
+gsap.to("#favg",{
+    scrollTrigger:{
+        scrub:true,
+        toggleaction:"play pause reverse pause"
+    },
+    scale:2.5,
+    opacity:9,
+    left:-500,
+})
+gsap.to("#hulk",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#hulk",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:-200,
+    opacity:9,
+})
+gsap.to("#thor",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#thor",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:-200,
+    opacity:9,
+})
+gsap.to("#cpmr",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#cpmr",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:-200,
+    opacity:9,
+})
+gsap.to("#inwr",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#inwr",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:-200,
+    opacity:9,
+})
+ gsap.to("#spfr",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#spfr",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:2,
+    left:-200,
+    opacity:9,
+})
+gsap.to("#irn1",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#irn1",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:500,
+})
+gsap.to("#irn2",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#irn2",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:500,
+})
+gsap.to("#wasp",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#wasp",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:500,
+})
+gsap.to("#ragk",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#ragk",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:500,
+})
+gsap.to("#edgm",{
+    scrollTrigger:{
+        scrub:true,
+        trigger:"#edgm",
+        toggleaction:"play pause reverse pause"
+    },
+    scale:1,
+    left:500,
+})
